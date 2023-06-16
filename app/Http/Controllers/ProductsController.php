4fa7819f1e0799f829a57ca37eefbef8c2d6dae8
Jacqueline Santillan <jacqueline.santillan30@gmail.com>
2023-06-16 12:13:11 -0600

    Nueva columna image_path en tabla Products, funciones en ProductsController

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,84 +3,85 @@
 namespace App\Http\Controllers;
 
 use App\Models\Products;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreProductsRequest;
 use App\Http\Requests\UpdateProductsRequest;
 
 class ProductsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function show(Request $request){
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+    
+    public function store(Request $request){
+        try {
+            $product = new Products;
+
+            $product->barcode    =  $request->barcode;
+            $product->name       =  $request->name;
+            $product->brand      =  $request->brand;
+            $product->price      =  $request->price;
+            $product->unit       =  $request->unit;
+            $product->stock      =  $request->stock;
+            $product->status     =  $request->status;
+            $pages->created_at   =  date("d-m-Y h:i:s");
+            $product->updated_at =  date("d-m-Y h:i:s");
+
+            $product->save();
+
+            return array(  
+                'error'   => false, 
+                'message' => 'Se almacenó el producto correctamente.', 
+                'code'    => 200
+            );
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    public function update(Request $request){
+        try {
+            $product = Products::find($request->id);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreProductsRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreProductsRequest $request)
-    {
-        //
-    }
+            $product->name       =  $request->name;
+            $product->brand      =  $request->brand;
+            $product->price      =  $request->price;
+            $product->unit       =  $request->unit;
+            $product->stock      =  $request->stock;
+            $product->status     =  $request->status;
+            $product->updated_at =  date("d-m-Y h:i:s");
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Products  $products
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Products $products)
-    {
-        //
-    }
+            $product->save();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Products  $products
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Products $products)
-    {
-        //
-    }
+            return array(  
+                'error'   => false, 
+                'message' => 'Se actualizó el producto correctamente.', 
+                'code'    => 200
+            );
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateProductsRequest  $request
-     * @param  \App\Models\Products  $products
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateProductsRequest $request, Products $products)
-    {
-        //
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
+    
+    public function destroy(Request $request){
+        try {
+            $product = Products::find($request->id);
+            $product->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Products  $products
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Products $products)
-    {
-        //
+            return array(  
+                'error'   => false, 
+                'message' => 'Se eliminó el producto correctamente.', 
+                'code'    => 200
+            );
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
