09eed13414304e5cfa6fb4b7476b94b3e775ff6e
Jacqueline Santillan <jacqueline.santillan30@gmail.com>
2023-06-16 11:37:50 -0600

    Creacion de migraciones

---
+++ database/migrations/2023_06_16_172353_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('barcode')->nullable();
+            $table->string('name', 200)->nullable();
+            $table->string('brand', 200)->nullable();
+            $table->string('price', 200)->nullable();
+            $table->string('unit', 200)->nullable();
+            $table->string('stock')->nullable();
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
